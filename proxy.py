4e40f55b56e3f428257cde5d183c01a480838c13
lexsos <ale-sosnin@mail.ru>
2018-02-09 12:09:55 +0300

    :rocket: add proxy filter

--- proxy.py
+++ proxy.py
@@ -2,6 +2,13 @@ import re
 import itertools
 import requests
 import time
+from urllib.parse import urlencode
+from multiprocessing import Pool
+
+
+PROCESSES = 32
+TEST_URL = 'https://www.reformagkh.ru/myhouse'
+TEST_TIMEOUT = 5
 
 
 class ProxyList(object):
@@ -55,16 +62,64 @@ class GatherList(ProxyList):
         return proxies
 
 
+class FoxtoolsScraper(object):
+
+    API_URL = 'http://api.foxtools.ru/v2/Proxy'
+    PER_PAGE = 100
+    MAX_PAGE=10
+
+    def __init__(self):
+        self.proxies = list()
+
+    def get_url(self, page):
+        arguments = urlencode(dict(free='yes', limit=100, page=page))
+        return '{}?{}'.format(self.API_URL, arguments)
+
+    def scrap(self):
+        for i in range(1, self.MAX_PAGE):
+            url = self.get_url(page=i)
+            try:
+                res = requests.get(url, timeout=5)
+                self.add_proxy_to_list(data=res.json())
+            except:
+                pass
+        return self.proxies
+
+    def add_proxy_to_list(self, data):
+        for item in data['response']['items']:
+            proxy = '{}:{}'.format(item['ip'], item['port'])
+            self.proxies.append(proxy)
+
+
 def grab_list():
-    for PrxClass in (SamairList, GatherList):
+    for PrxClass in (SamairList, GatherList, FoxtoolsScraper):
         prx = PrxClass()
         prx.scrap()
         for proxy in prx.proxies:
             yield proxy
 
 
+def test_proxy(proxy_address):
+    try:
+        proxy = {'https': 'http://' + proxy_address.strip('\n')}
+        requests.get(TEST_URL, proxies=proxy, timeout=TEST_TIMEOUT)
+        print(proxy_address)
+        return proxy_address
+    except:
+        pass
+
+
+def proxy_filter(proxy_list):
+    with Pool(PROCESSES) as p:
+        tested_proxy = p.map(test_proxy, proxy_list)
+    return [p for p in tested_proxy if p]
+
+
 if __name__ == '__main__':
 
+    full_list = grab_list()
+    good_list = proxy_filter(full_list)
+
     with open('proxylist.txt', 'w') as outfile:
-        for row in grab_list():
+        for row in good_list:
             outfile.write('{}\n'.format(row))
